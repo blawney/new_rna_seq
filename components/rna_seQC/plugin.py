2b7fae3460d9e27019d6fd26619bb8e48e1cb917
Brian Lawney <blawney@scribbles.dfci.harvard.edu>
2015-04-02 21:09:01 -0400

    Completing implementation of rna-seQC module.  Still need tests.

--- components/rna_seQC/plugin.py
+++ components/rna_seQC/plugin.py
@@ -38,46 +38,42 @@ def run(project):
 
 def run_qc(project, util_methods):
 
-	#TODO: write the base cmd:
-	'''
-			java -jar $RNA_SEQC_JAR \
-			-o $SAMPLE_QC_DIR \
-			-r $GENOMEFASTA \
-			-s "$SAMPLE|$SAMPLE_BAM|-" \
-			-t $GTF_FOR_RNASEQC
-	'''
-	base_command = ''
+	base_command = 'java -jar ' + project.parameters.get('rnaseqc_jar') 
+	base_command +=' -r ' + project.parameters.get('genome_fasta')
+	base_command +=' -t ' + project.parameters.get('rnaseqc_gtf')
 
 	for sample in project.samples:
 		# only use the most 'raw' bamfile at this point.
 		bamfile = get_earliest_version_of_file(sample.bamfiles)
-			if os.path.isfile(bamfile):
+		if os.path.isfile(bamfile):
 
-				# make output directory for this BAM file's QC:
-				name = util_methods.case_insensitive_rstrip(os.path.basename(bamfile), 'bam')
-				output_dir = os.path.join(project.parameters.get('rnaseqc_output_dir'), name)
-				util_methods.create_directory(output_dir)
+			# make output directory for this BAM file's QC:
+			name = util_methods.case_insensitive_rstrip(os.path.basename(bamfile), 'bam')
+			output_dir = os.path.join(project.parameters.get('rnaseqc_output_dir'), name)
+			util_methods.create_directory(output_dir)
 
-				# TODO: complete the call
-				command = base_command + ''
+			arg = '"' + sample.sample_name + '|' + bamfile + '|-"'
 
-				logging.info('Calling rnaSeQC with: ')
-				logging.info(command)
-				process = subprocess.Popen(command, shell = True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+			command = base_command + ' -o ' + output_dir + ' -s ' + arg
 
-				stdout, stderr = process.communicate()
-				logging.info('STDOUT from rna-SeQC: ')
-				logging.info(stdout)
-				logging.info('STDERR from rna-SeQC: ')
-				logging.info(stderr)
+			logging.info('Calling rnaSeQC with: ')
+			logging.info(command)
+			process = subprocess.Popen(command, shell = True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+
+			stdout, stderr = process.communicate()
+			logging.info('STDOUT from rna-SeQC: ')
+			logging.info(stdout)
+			logging.info('STDERR from rna-SeQC: ')
+			logging.info(stderr)
 	
-				if process.returncode != 0:			
-					logging.error('There was an error encountered during execution of rna-SeQC for sample %s ' % sample.sample_name)
-					raise Exception('Error during rna-SeQC module.')
+			if process.returncode != 0:			
+				logging.error('There was an error encountered during execution of rna-SeQC for sample %s ' % sample.sample_name)
+				raise Exception('Error during rna-SeQC module.')
 
-			else:
-				logging.error('The bamfile (%s) is not actually a file.' % bamfile)
-					raise MissingBamFileException('Missing BAM file: %s' % bamfile)
+		else:
+			logging.error('The bamfile (%s) is not actually a file.' % bamfile)
+			raise MissingBamFileException('Missing BAM file: %s' % bamfile)
+
 
 
 def get_earliest_version_of_file(file_list):
